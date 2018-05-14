92d3c47d4670ec1bbfa76b3326487b60102b9df4
Vasiliy Vdovkin <svvatsteam@gmail.com>
2018-05-15 00:58:59 +0300

    update thread removal

--- app/server/server.py
+++ app/server/server.py
@@ -42,7 +42,7 @@ def index():
 
 @app.route('/<int:topic_count>')
 def get_content(topic_count=5):
-    global events
+    events = db.events.find({})
     if len(events) < topic_count:
         return redirect(
             url_for('get_content', topic_count=len(events)))
@@ -53,7 +53,7 @@ def get_content(topic_count=5):
 
 @app.route('/api/<int:offset>')
 def api_get_content(offset=app.config['OFFSET']):
-    global events
+    events = db.events.find({})
     if offset >= len(events):
         response = jsonify(message="Topic number too large")
         response.status_code = 404
@@ -69,22 +69,8 @@ def reverse_filter(s):
     return min(s)
 
 
-def update_events():
-    global events
-    while True:
-        if db.events.count():
-            events = list(db.events.find())
-            logger.info('Server get {} events'.format(len(events)))
-        time.sleep(app.config['UPDATE_RATE'])
-
-
 if __name__ == "__main__":
-    global events
-    events = []
-    thread = Thread(target=update_events)
-    thread.start()
-    logger.info('started')
-    while not events:
+    while not db.events.count():
         logger.info('Waiting for data... {}'.format(db.events.count()))
         time.sleep(app.config['BOOTSTRAP_WAIT'])
     app.run(host='0.0.0.0', port=app.config['PORT'], threaded=True, use_reloader=False)
