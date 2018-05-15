2777966a1958cf664ae1cdc32a27efbd96e072b8
Vasiliy Vdovkin <svvatsteam@gmail.com>
2018-05-15 04:11:17 +0300

    debugging in prod env like pro

--- app/server/server.py
+++ app/server/server.py
@@ -47,7 +47,7 @@ def get_content(topic_count=5):
         return redirect(
             url_for('get_content', topic_count=len(events)))
     events_sliced = events[:topic_count]
-    count = sum(len(x['content'] for x in events))
+    count = sum(len(x['content']) for x in events)
     return render_template('main.html', groups_count=len(events),
                            count=count, groups=events_sliced)
 
